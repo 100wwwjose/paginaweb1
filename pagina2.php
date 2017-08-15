ca4f6d5bf84368eda5a04eb27ad84f7a56e07159
Jose Perfecto Figueroa Sánchez <josplatina@gmail.com>
2017-08-15 08:33:25 -0500

    Primero entramos a la carpeta raiz proyectos, despues copiamos el archivo index a la carpeta proyectos  y revisamos que estubiera dado de alta, posteriormente creamos una segunda pagina y por ultimocreamos enlaces entre las paginas

---
+++ pagina2.php
@@ -0,0 +1,82 @@
+<html>
+	<head>
+		<title>Programacion visual</title>
+		<style type="text/css">
+			
+			* {
+				margin:0px;
+				padding:0px;
+			}
+			
+			#header {
+				margin:auto;
+				width:2000px;
+				font-family:Arial, Helvetica, sans-serif;
+			}
+			
+			ul, ol {
+				list-style:none;
+			}
+			
+			.nav > li {
+				float:left;
+			}
+			
+			.nav li a {
+				background-color:#000;
+				color:#fff;
+				text-decoration:none;
+				padding:10px 12px;
+				display:block;
+			}
+			
+			.nav li a:hover {
+				background="111"
+			}
+			
+			.nav li ul {
+				display:none;
+				position:absolute;
+				min-width:140px;
+			}
+			
+			.nav li:hover > ul {
+				display:block;
+			}
+			
+			.nav li ul li {
+				position:relative;
+			}
+			
+			.nav li ul li ul {
+				right:-140px;
+				top:0px;
+			}
+			
+		</style>
+	</head>
+	<body>
+	
+	<body background="fondo.jpg">
+		<div id="header">
+			<ul class="nav">
+			</ul>
+		</div>
+		<p>
+		         <br>
+		         <h1> _____________________________________________________</h1>
+				 <br>
+				 <font COLOR="Silver">
+				 <h1 ALIGN=center> UNIVERSIDAD AUTONOMA DEL ESTADO DE MEXICO </h1>
+				 <br>
+	             <h1 ALIGN=center> CENTRO UNIVERSITARIO UAEM ATLACOMULCO </h1>
+	             <br>
+	             <h1 ALIGN=center> INGENIERIA EN COMPUTACION </h1>
+				 <br>
+	             <h1 ALIGN=center> Pagina 2  </h1>
+				 <br>
+	             <h1 ALIGN=center> LENGUAJE DE PROGRAMACION VISUAL </h1>
+				 </font>
+		</p>
+	</body>
+</html>
